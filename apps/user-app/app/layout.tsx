f65aaeb326ec2b30f8bb5489e264ab32515ca0c4
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-12-25 01:53:30 +0530

    App added

---
+++ apps/user-app/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Providers } from "../provider";
+
+export const metadata: Metadata = {
+  title: "User App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang="en">
+      <Providers>
+        <body>{children}</body>
+      </Providers>
+    </html>
+  );
+}
